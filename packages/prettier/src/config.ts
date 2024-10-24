a10895d1077e4ac548165c3f20cd2d894eabf343
Ty Eggen <tycho.eggen@gmail.com>
2024-10-23 22:38:44 -0700

    feat(prettier): ✨ initial prettier config

---
+++ packages/prettier/src/config.ts
@@ -0,0 +1,16 @@
+export default {
+    arrowParens: 'always',
+    bracketSameLine: false,
+    bracketSpacing: true,
+    embeddedLanguageFormatting: 'auto',
+    htmlWhitespaceSensitivity: 'strict',
+    jsxSingleQuote: true,
+    proseWrap: 'preserve',
+    quoteProps: 'consistent',
+    semi: false,
+    singleAttributePerLine: false,
+    singleQuote: true,
+    trailingComma: 'none',
+    plugins: ['prettier-plugin-sh', 'prettier-plugin-tailwindcss'],
+    tailwindFunctions: ['clsx']
+}
